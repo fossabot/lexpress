a35d840352b78356717e90c4fa87df7996c43024
Ivan Gabriele <ivan.gabriele@gmail.com>
2017-10-15 00:08:58 +0200

    dec: fix index path

---
+++ index.ts
@@ -0,0 +1,21 @@
+import {
+  BaseControllerMethod,
+  BaseControllerResponse,
+  BaseControllerConstructor,
+  Route,
+  LexpressOptions,
+} from './src'
+
+export {
+  BaseControllerMethod,
+  BaseControllerResponse,
+  BaseControllerConstructor,
+  Route,
+  LexpressOptions,
+}
+
+import Lexpress from './src/Lexpress'
+export default Lexpress
+
+import BaseController from './src/BaseController'
+export { BaseController }
