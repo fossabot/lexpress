4481c51f146e30ad7895a620a71ffcccce7119b5
Ivan Gabriele <ivan.gabriele@gmail.com>
2017-10-15 01:27:42 +0200

    webpack: create (for test purpose)

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  entry: './index.ts',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  module: {
+    loaders: [
+      { test: /.*\.ts$/, loader: 'ts-loader', exclude: /node_modules/ },
+    ],
+  },
+}
