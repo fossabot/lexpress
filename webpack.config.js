67c3a84a0ac97b8d8a48b0752949e243824b331c
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-15 11:06:24 +0100

    webpack: lint

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,25 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.ts',
+
   target: 'node',
+
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
     libraryTarget: 'commonjs',
   },
+
   resolve: {
     extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
   },
+
   module: {
     loaders: [
       { test: /.*\.ts$/, loader: 'awesome-typescript-loader' },
     ],
   },
+
   plugins: [
     new CleanWebpackPlugin(['build']),
   ],
