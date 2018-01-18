6ba7af35a0325b1ecb4949a8bcb65928b39775a2
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-01-18 14:42:01 +0100

    libs/media/logo: rename meta to __LEXPRESS_VERSION__

--- src/libs/media/logo.ts
+++ src/libs/media/logo.ts
@@ -1,7 +1,7 @@
 import chalk from 'chalk'
 
 // Is replaced with postversion script
-const VERSION: string = `__VERSION__`
+const VERSION: string = `__LEXPRESS_VERSION__`
 
 export default chalk.gray(`
 ,
