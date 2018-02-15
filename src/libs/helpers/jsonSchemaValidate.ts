4cb0604b5033c6e5c6f88599611240f3483d312a
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-16 00:14:57 +0100

    libs/helpers/jsonSchemaValidate: lint

--- src/libs/helpers/jsonSchemaValidate.ts
+++ src/libs/helpers/jsonSchemaValidate.ts
@@ -1,9 +1,10 @@
 import * as Ajv from 'ajv'
 
+// tslint:disable-next-line:ban-types
 export type Schema = Object | string | boolean
 export type Callback = (err: string | null) => any
 
-const ajv = new Ajv()
+const ajv: Ajv.Ajv = new Ajv()
 
 export default function(schema: Schema, data: {}, cb: Callback): void {
   cb(ajv.validate(schema, data) ? null : ajv.errorsText())
