1052a10e2d5a1e69598b713e566559a187ce0216
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-15 23:49:54 +0100

    index: import all global types

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,44 @@ export { BaseController }
 import log from './libs/helpers/log'
 export { log }
 
-import * as Types from './types'
-export { Types }
+import * as express from 'express'
+import { ServerOptions } from 'https'
+
+export interface Response extends express.Response {
+  cache?: (forInSeconds: number) => CacheResponse
+}
+export interface CacheResponse {
+  json: express.Response['json']
+  render: express.Response['render']
+}
+
+export type BaseControllerMethod = 'delete' | 'get' | 'post' | 'put'
+
+export type BaseControllerResponse = Response | Promise<Response> | void
+
+export interface BaseControllerConstructor {
+  new (req: express.Request, res: express.Response): BaseController
+}
+
+export interface Route {
+  call?: any
+  controller?: BaseControllerConstructor
+  method: BaseControllerMethod
+  path: string
+  settings?: {
+    isCached?: boolean
+  }
+}
+
+export interface LexpressOptions {
+  headers?: LexpressOptionsHeaders
+  https?: false | ServerOptions
+  middlewares?: express.RequestHandler[]
+  routes: Route[]
+  viewsEngine?: 'mustache'/* | 'pug'*/
+  viewsPath?: string
+}
+export type LexpressOptionsHeaders = {
+  'Access-Control-Allow-Origin'?: string
+  'Access-Control-Allow-Headers'?: string
+}
