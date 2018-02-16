3d6757642600175b7c5b8d864f5dc8a025c31acb
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-16 02:25:52 +0100

    0.31.1

--- build/index.d.ts
+++ build/index.d.ts
@@ -28,7 +28,7 @@ export interface Route {
     call?: any;
     controller?: BaseControllerConstructor;
     method: BaseControllerMethod;
-    middleware: express.RequestHandler;
+    middleware?: express.RequestHandler;
     path: string;
     settings?: {
         isCached?: boolean;
