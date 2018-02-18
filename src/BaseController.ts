708262a202b8e6a02c87bea1d2ea357a92f2ed03
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-02-18 02:30:50 +0100

    BaseController: handle non-get JsonSchema validations

--- src/BaseController.ts
+++ src/BaseController.ts
@@ -4,13 +4,12 @@ import { Request } from 'express'
 import answerError from './libs/helpers/answerError'
 import jsonSchemaValidate, { Schema } from './libs/helpers/jsonSchemaValidate'
 
-import { BaseControllerResponse, Response } from '.'
-
-const HTTP_STATUS_CODE_NOT_FOUND: number = 404
+import { BaseControllerMethod, BaseControllerResponse, Response } from '.'
 
 export default abstract class BaseController {
   protected readonly controllerName: string = this.constructor.name
   protected isJson: boolean = true
+  protected method: BaseControllerMethod
   protected readonly req: Request
   protected readonly res: Response
 
@@ -20,16 +19,16 @@ export default abstract class BaseController {
   }
 
   public get(): BaseControllerResponse {
-    this.answerError('Not Found', HTTP_STATUS_CODE_NOT_FOUND)
+    this.method = 'get'
   }
   public post(): BaseControllerResponse {
-    this.answerError('Not Found', HTTP_STATUS_CODE_NOT_FOUND)
+    this.method = 'post'
   }
   public put(): BaseControllerResponse {
-    this.answerError('Not Found', HTTP_STATUS_CODE_NOT_FOUND)
+    this.method = 'put'
   }
   public delete(): BaseControllerResponse {
-    this.answerError('Not Found', HTTP_STATUS_CODE_NOT_FOUND)
+    this.method = 'delete'
   }
 
   protected log(message: string): void {
@@ -57,7 +56,7 @@ export default abstract class BaseController {
   protected validateJsonSchema(schema: Schema, cb: () => BaseControllerResponse): void {
     this.log(`Validating JSON Schema`)
 
-    jsonSchemaValidate(schema, this.req.query, (err: string) => {
+    jsonSchemaValidate(schema, this.method === 'get' ? this.req.query : this.req.body, (err: string) => {
       if (err !== null) {
         this.answerError(err)
 
